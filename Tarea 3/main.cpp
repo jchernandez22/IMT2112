1bde9888d3a8d86fb721869979875d1963cd395c
Juani <jchernandez2@uc.cl>
2021-11-05 21:33:47 -0300

    Cambio

--- Tarea 3/main.cpp
+++ Tarea 3/main.cpp
@@ -36,10 +36,10 @@ double calculateC(double i, double j, double h_x, double h_y, int firstIndex){
     return -calculateW(i,j,h_x,h_y,firstIndex) - calculateE(i,j,h_x,h_y,firstIndex) - calculateS(i,j,h_x,h_y,firstIndex) - calculateN(i,j,h_x,h_y,firstIndex) + 1;
 }
 
-void print_2DArray(double *array, int n, int m) {
+void print_2DArray(double *array, int m, int n) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            cout << *((array+i*m) + j) << " ";
+            cout << *((array+i*n) + j) << " ";
         }
         cout << endl;
   }
