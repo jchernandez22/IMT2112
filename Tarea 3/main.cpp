b37eaa20362bdbba47613c5c87f849657cab5fdc
Juani <jchernandez2@uc.cl>
2021-11-05 21:07:56 -0300

    Cambio

--- Tarea 3/main.cpp
+++ Tarea 3/main.cpp
@@ -36,7 +36,7 @@ double calculateC(double i, double j, double h_x, double h_y, int firstIndex){
     return -calculateW(i,j,h_x,h_y,firstIndex) - calculateE(i,j,h_x,h_y,firstIndex) - calculateS(i,j,h_x,h_y,firstIndex) - calculateN(i,j,h_x,h_y,firstIndex) + 1;
 }
 
-void print_matrix(double** matrix, int n, int m) {
+void print_matrix(double* matrix, int n, int m) {
   printf("\n");
   for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
